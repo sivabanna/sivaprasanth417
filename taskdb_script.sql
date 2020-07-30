4d4c557d19aa7ed6e092e993144e275bea162e3b
sivabanna <54401019+sivabanna@users.noreply.github.com>
2020-07-30 20:42:32 +0530

    Create taskdb_script.sql

---
+++ taskdb_script.sql
@@ -0,0 +1,80 @@
+-- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+--
+-- Host: localhost    Database: accountdb
+-- ------------------------------------------------------
+-- Server version	8.0.20
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `account_details`
+--
+
+DROP TABLE IF EXISTS `account_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `account_details` (
+  `sl.no` int NOT NULL AUTO_INCREMENT,
+  `account_no` int NOT NULL,
+  `holder_name` varchar(50) NOT NULL,
+  `balance` int NOT NULL,
+  PRIMARY KEY (`sl.no`),
+  UNIQUE KEY `sl.no_UNIQUE` (`sl.no`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `account_details`
+--
+
+LOCK TABLES `account_details` WRITE;
+/*!40000 ALTER TABLE `account_details` DISABLE KEYS */;
+INSERT INTO `account_details` VALUES (1,32021056,'Gowri',32000),(2,32021457,'vijaya',45000),(3,32023568,'prabha',21000);
+/*!40000 ALTER TABLE `account_details` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `account_transactions`
+--
+
+DROP TABLE IF EXISTS `account_transactions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `account_transactions` (
+  `transaction_id` int NOT NULL AUTO_INCREMENT,
+  `transaction_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `amount` int NOT NULL,
+  `to_account` int NOT NULL,
+  PRIMARY KEY (`transaction_id`),
+  UNIQUE KEY `transaction_id_UNIQUE` (`transaction_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1010 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `account_transactions`
+--
+
+LOCK TABLES `account_transactions` WRITE;
+/*!40000 ALTER TABLE `account_transactions` DISABLE KEYS */;
+INSERT INTO `account_transactions` VALUES (234,'2020-07-25 14:29:36',500,32021457),(247,'2020-07-26 08:45:13',1000,32021457),(256,'2020-07-26 15:23:30',200,32023568),(286,'2020-07-29 00:46:25',600,32023568),(1001,'2020-07-29 13:27:36',250,32023568),(1002,'2020-07-29 13:28:53',1000,666655552),(1003,'2020-07-29 13:32:15',1000,666655552),(1004,'2020-07-29 13:36:32',1000,666655552),(1005,'2020-07-29 13:39:20',1000,666655552),(1006,'2020-07-29 13:50:41',1000,32023568),(1007,'2020-07-29 13:55:33',1000,666655551),(1008,'2020-07-29 13:56:22',1000,666655551),(1009,'2020-07-29 14:08:12',1000,666655586);
+/*!40000 ALTER TABLE `account_transactions` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
